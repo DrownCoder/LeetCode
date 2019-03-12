870ecce94ebc8d3535d5d09d892dc624e61c6160
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-03-12 21:35:27 +0800

    链表成环

--- src/must/QAList.java
+++ src/must/QAList.java
@@ -29,21 +29,45 @@ class QAList {
     }
 
     public ListNode reverseList2(ListNode head) {
-        if(head == null||head.next==null){
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode reNode=reverseList(head.next);
-        head.next.next=head;
-        head.next=null;
+        ListNode reNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
         return reNode;
     }
 
+    /**
+     * 快慢指针，快指针和慢指针，快的每次多走一步，如果相遇则有环，没有相遇到null，则说明有结尾，说明不是环
+     */
+    public boolean isCircle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode low = head;
+        while (fast != null && low != null) {
+            if (fast == low) {
+                return true;
+            }
+            fast = fast.next;
+            low = low.next;
+            if (fast != null) {
+                fast = fast.next;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 
-    class ListNode{
+
+    class ListNode {
         int data;
         ListNode next;
 
-        public ListNode(int data){
+        public ListNode(int data) {
             this.data = data;
         }
     }
