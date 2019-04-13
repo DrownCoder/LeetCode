dcd47300596b537080fbdb05214148a8a57f817c
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-04-13 13:59:57 +0800

    数组删除重复项

---
+++ src/QA14.java
@@ -0,0 +1,31 @@
+/**
+ * Author : xuan.
+ * Date : 2019-04-13.
+ * Description :给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 26
+ */
+class QA14 {
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 1, 2, 4, 5};
+
+        removeDuplicates(a);
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+}
