1414933795db1b2327c37c5b263eb03b24e830a2
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-04-16 21:18:56 +0800

    归并排序

--- src/must/Sort.java
+++ src/must/Sort.java
@@ -12,7 +12,8 @@ class Sort {
         //bubbleSort(arr);
         //selectSort(arr);
         //insertSort(arr);
-        xierSort(arr);
+        //xierSort(arr);
+        MergeSort(arr);
         printArr(arr);
     }
 
@@ -180,7 +181,32 @@ class Sort {
     private static void mergeSort(int[] arr, int left, int right, int[] temp) {
         if (left < right) {
             int mid = (left + right) / 2;
-            //sort(arr,)
+            mergeSort(arr, left, mid, temp);
+            mergeSort(arr, mid + 1, right, temp);
+            merge(arr, left, mid, right, temp);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+        int t = 0;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[t++] = arr[i++];
+            } else {
+                temp[t++] = arr[j++];
+            }
+        }
+        while (i <= mid) {
+            temp[t++] = arr[i++];
+        }
+        while (j <= right) {
+            temp[t++] = arr[j++];
+        }
+        t = 0;
+        while (left <= right) {
+            arr[left++] = temp[t++];
         }
     }
 
