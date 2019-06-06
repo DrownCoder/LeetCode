0f6e447cf64bf3799d5405d21865afd3cdfb3cf6
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-06-06 11:51:29 +0800

    两个队列实现一个栈

--- src/QA21.java
+++ src/QA21.java
@@ -4,21 +4,30 @@ import java.util.Stack;
  * Author : xuan.
  * Date : 2019-06-05.
  * Description :两个栈实现一个队列，两个队列实现一个栈
+ * https://www.cnblogs.com/tracyhan/p/5490775.html
  */
 class QA21 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5};
         Queue queue = new Queue();
+        StackQ stackQ = new StackQ();
+
         for (int i : arr) {
             queue.push(i);
+            stackQ.push(i);
         }
+
         while (!queue.isEmpty()) {
             System.out.print(queue.pop());
         }
+
+        while (!stackQ.isEmpty()) {
+            System.out.print(stackQ.pop());
+        }
     }
 
-    public static class Queue {
-        private Stack<Integer> stack1, stack2;
+    public static class Queue<T> {
+        private Stack<T> stack1, stack2;
 
 
         public Queue() {
@@ -26,11 +35,11 @@ class QA21 {
             this.stack2 = new Stack<>();
         }
 
-        public void push(int value) {
+        public void push(T value) {
             stack1.push(value);
         }
 
-        public int pop() {
+        public T pop() {
             if (stack2.size() > 0) {
                 return stack2.pop();
             } else {
@@ -43,6 +52,44 @@ class QA21 {
 
         public boolean isEmpty() {
             return stack1.isEmpty() && stack2.isEmpty();
+        }
+
+        public int size() {
+            return stack1.size() > 0 ? stack1.size() : stack2.size();
+        }
+    }
+
+
+    public static class StackQ<T> {
+        public Queue<T> queue1, queue2;
+
+        public StackQ() {
+            queue1 = new Queue<>();
+            queue2 = new Queue<>();
+        }
+
+        public void push(T value) {
+            queue1.push(value);
+        }
+
+        public T pop() {
+            int size = queue1.size();
+            for (int i = 0; i < size - 1; i++) {
+                queue2.push(queue1.pop());
+            }
+            T result = queue1.pop();
+            while (!queue2.isEmpty()) {
+                queue1.push(queue2.pop());
+            }
+            return result;
+        }
+
+        public int size() {
+            return queue1.size() > 0 ? queue1.size() : queue2.size();
+        }
+
+        public boolean isEmpty() {
+            return queue1.isEmpty();
         }
     }
 }
