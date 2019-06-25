6ea87db3afa2ab7c482bf64b350e1ad8dd43db9c
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-06-25 10:53:07 +0800

    236. 二叉树的最近公共祖先

---
+++ src/QA25.java
@@ -0,0 +1,32 @@
+/**
+ * Author : xuan.
+ * Date : 2019-06-25.
+ * Description :236. 二叉树的最近公共祖先
+ */
+class QA25 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (q == root || p == root) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else {
+            return left == null ? right : left;
+        }
+    }
+}
