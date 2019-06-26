b8effe116cb7bb0a42bfdc9a65c4c9b959e1c6a2
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-06-26 19:44:31 +0800

    二维数组查找数

--- src/QA3.java
+++ src/QA3.java
@@ -5,7 +5,6 @@
  * <p>
  * 每行的元素从左到右升序排列。
  * 每列的元素从上到下升序排列。
- *
  */
 class QA3 {
     public static void main(String[] args) {
@@ -32,7 +31,32 @@ class QA3 {
             }
             if (matrix[startL][startC] > target) {
                 startC--;
-            }else {
+            } else {
+                startL++;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 从右上角开始遍历，效率高
+     */
+    public boolean search2Array(int[][] martix, int target) {
+        if (martix == null || martix.length == 0 || martix[0].length == 0) {
+            return false;
+        }
+        int startC = martix[0].length - 1;
+        int startL = 0;
+        if (martix[startL][startC] == target) {
+            return true;
+        }
+        while (startL < martix.length && startC < martix[0].length) {
+            if (martix[startL][startL] == target) {
+                return true;
+            }
+            if (martix[startL][startC] > target) {
+                startC--;
+            }else{
                 startL++;
             }
         }
