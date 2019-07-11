ba6c540ebe0dc989087c9c98f7ef28e102379931
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-07-11 20:50:35 +0800

    分割回文串

--- src/QA7.java
+++ src/QA7.java
@@ -4,31 +4,52 @@ import java.util.List;
 /**
  * Author : xuan.
  * Date : 2019-03-08.
- * Description :给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
- *
+ * Description :131.给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+ * <p>
  * 返回 s 所有可能的分割方案。
- *
  */
 class QA7 {
-    public List<List<String>> partition(String s) {
-        char[] chars = s.toCharArray();
-        StringBuilder str = new StringBuilder();
-        List<List<String>> results = new ArrayList<>();
-        for (int i = 0; i < chars.length; i++) {
-            List<String> result = new ArrayList<>();
-            for (int t = 0; t < chars.length; t += i) {
-                for (int m = 0; m <= i; m++) {
-                    str.append(chars[t + m]);
-                }
-                if (isPalindrome(str.toString())) {
-                    result.add(str.toString());
-                }
-            }
-            if (result.size() != 0) {
-                results.add(result);
+    private static List<List<String>> res = new ArrayList<>();
+    private static String ss;
+
+    public static void main(String[] args) {
+        partition("aab");
+    }
+
+    /**
+     * 基本的回溯法，有点像动态规划，先求前面的回文字符串，然后加上剩余子字符串的回文字符串。
+     */
+    public static List<List<String>> partition(String s) {
+        if (s == null || s.length() == 0) {
+            return res;
+        }
+        ss = s;
+        dfs(new ArrayList<>(), 0);
+        return res;
+    }
+
+    public static void dfs(List<String> ll, int index) {
+        if (index == ss.length()) {
+            res.add(new ArrayList<>(ll));
+            return;
+        }
+        for (int i = index; i < ss.length(); i++) {
+            if (isPalindrome(index, i)) {
+                ll.add(ss.substring(index, i + 1));
+                dfs(ll, i + 1);
+                ll.remove(ll.size() - 1);
             }
         }
-        return results;
+    }
+
+    public static boolean isPalindrome(int start, int end) {
+        while (start < end) {
+            if (ss.charAt(start) != ss.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 
     public static boolean isPalindrome(String s) {
