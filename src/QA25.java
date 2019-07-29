226f45e7836611fd8c13494ed6cb2cb9d3dceba3
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-07-29 10:38:57 +0800

    复习25

--- src/QA25.java
+++ src/QA25.java
@@ -29,4 +29,24 @@ class QA25 {
             return left == null ? right : left;
         }
     }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == q || root == p) {
+            return root;
+        }
+        //左边找两个的公共父节点
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        //右边找两个的公共父节点
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        if (left != null && right != null) {
+            //两个都不为null,则说明左右都能找到，由唯一性，确定为root
+            return root;
+        }else{
+            //任意一个不为null,说明在一端可以找到，那么就是那个节点
+            return left != null ? left : right;
+        }
+    }
 }
