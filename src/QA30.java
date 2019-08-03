9cd5b43676979a33c240dfe0d949bf62ba570d10
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-08-03 10:37:37 +0800

    复习30

--- src/QA30.java
+++ src/QA30.java
@@ -32,5 +32,34 @@ class QA30 {
             slow.next = slow.next.next;
             return head;
         }
+
+        /**
+         * 快慢指针
+         * 1-2-3-4-5
+         * 倒数第二个
+         * fast先走n步
+         * fast->2
+         * slow->1
+         * slow和fast同时走，走到末尾
+         * fast->5
+         * slow->3
+         */
+        public ListNode removeNthFromEnd2(ListNode head, int n) {
+            ListNode fast = head;
+            while (fast != null && n > 0) {
+                fast = fast.next;
+                n--;
+            }
+            if (fast == null) {
+                return head;
+            }
+            ListNode slow = head;
+            while (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return head;
+        }
     }
 }
