b484c423142a830a5095fc8a2775039cec038c97
dengzhaoxuan <dengzhaoxuan@dangdang.com>
2019-08-07 10:51:35 +0800

    复习View最大树深度

--- src/must/AndroidViewDeep.java
+++ src/must/AndroidViewDeep.java
@@ -29,6 +29,26 @@ class AndroidViewDeep {
         }
     }
 
+    public int getViewDeep2(View view, int height) {
+        if (view == null) {
+            return 0;
+        }
+        if (view instanceof ViewGroup) {
+            if (((ViewGroup) view).getChildCount() > 0) {
+                int max = height + 1;
+                for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                    int deep = getViewDeep2(((ViewGroup) view).getChildAt(i), height + 1);
+                    max = Math.max(max, deep);
+                }
+                return max;
+            } else {
+                return height;
+            }
+        }else{
+            return height;
+        }
+    }
+
     public static class ViewGroup extends View {
         public int getChildCount() {
             return 0;
